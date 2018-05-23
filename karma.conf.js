0fbe4b1ef0e9711454856707f48e4ee01cb687db
John Nguyen <hello@jnguyen.me>
2018-05-23 06:05:49 -0700

    Refine testing browsers

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,19 @@
+// Add chromeheadless bin path to the node environment
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function(config) {
 	config.set({
 		basePath: '',
-		frameworks: ['mocha', 'chai'],
-		files: [
-			'lib/*.js',
-			'test/*.js'
-		],
-		exclude: [
-		],
-		preprocessors: {
-		},
-		reporters: ['progress'],
+		frameworks: [ 'mocha', 'chai' ],
+		files: [ 'dist/*.js', 'test/*.js' ],
+		exclude: [],
+		preprocessors: {},
+		reporters: [ 'progress' ],
 		port: 9876,
 		colors: true,
 		logLevel: config.LOG_INFO,
 		autoWatch: true,
-		browsers: [
-			'Chrome',
-			'Firefox',
-			'Safari',
-			'IE'
-		],
+		browsers: [ 'ChromeHeadless', 'Firefox' ],
 		singleRun: true,
 		concurrency: Infinity
 	})
